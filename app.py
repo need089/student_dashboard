15c9fb24c25d9043d0cef9b2d001acad923e9bc8
need089 <6620117089@email.psu.ac.th>
2026-08-14 23:46:25 +0700

    Remove unused streamlit_option_menu import

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-from streamlit_option_menu import option_menu
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
